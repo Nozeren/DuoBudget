53a75df6ffd2656f1a5dd27ec87fe7a15d659ecb
Nozeren <45057714+Nozeren@users.noreply.github.com>
2025-02-04 19:21:15 +0200

    :sparkles: Add user settings

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 @app.route('/')
-async def upload():
+async def home():
     return render_template('home/index.html')
+
+@app.route('/settings/user')
+async def settings_user():
+    return render_template('settings/user.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
