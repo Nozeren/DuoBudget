24237526d2f736e7e7486d2d4e23c2e2d678b2e4
Nozeren <45057714+Nozeren@users.noreply.github.com>
2025-02-11 20:38:51 +0200

    :zap: Improve settings user, added save user elements

--- src/main.py
+++ src/main.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 async def home():
     return render_template('home/index.html')
 
-@app.route('/settings/user')
+@app.route('/settings/user', methods=['POST', 'GET'])
 async def settings_user():
+    if request.method == 'POST':
+        if request.form.get('inputusername') is not None:
+            print("Saved new user: " + request.form['inputusername'])
+        if request.form.get('useroption') is not None:
+            print("Removed new user: " + request.form['useroption'])
     return render_template('settings/user.html')
 
 if __name__ == "__main__":
--- src/static/script/settings/user.js
+++ src/static/script/settings/user.js
@@ -1,15 +1,22 @@
 let addUser = document.getElementById('adduser')
 let removeUser = document.getElementById('removeuser')
+let saveUser = document.getElementById('saveuser')
+let cancelUser = document.getElementById('canceluser')
 let newUserOption = document.getElementById('newuseroption')
 
 addUser.addEventListener('click', function(e){
     let checkInputUserName = document.getElementById('inputusername');
     if (checkInputUserName===null){
-    let inputUserName = document.createElement('input');
-    inputUserName.id = 'inputusername';
-    inputUserName.autofocus =true;
-    newUserOption.appendChild(inputUserName);
-    newUserOption.submit();
+        let inputUserName = document.createElement('input');
+        inputUserName.id = 'inputusername';
+        inputUserName.name = 'inputusername';
+        inputUserName.autofocus = true;
+        newUserOption.appendChild(inputUserName);
+        addUser.style.display = 'none';
+        removeUser.style.display = 'none';
+        saveUser.style.display = 'flex';
+        cancelUser.style.display = 'flex';
+        newUserOption.submit();
     }
 })
 
@@ -18,7 +25,20 @@ removeUser.addEventListener('click', function(e){
     for (const radio of radios){
         if (radio.checked){
             let div = radio.parentElement
-            div.remove();
+            //div.remove();
+            document.getElementById('userform').submit();
         }
     }
+})
+
+cancelUser.addEventListener('click', function(e){
+    saveUser.style.display = 'none';
+    cancelUser.style.display = 'none';
+    addUser.style.display = 'flex';
+    removeUser.style.display = 'flex';
+    let input= document.getElementById('inputusername');
+    input.remove();
+})
+saveUser.addEventListener('click', function(e){
+    document.getElementById('userform').submit();
 })
