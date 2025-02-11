a6c05b8d772c80460de5061f4004a422503ab811
Nozeren <45057714+Nozeren@users.noreply.github.com>
2025-02-11 17:07:07 +0200

    ⚡Improve user settings layout

--- src/static/script/settings/user.js
+++ src/static/script/settings/user.js
@@ -1,4 +1,24 @@
-function expandAddUser(){
-    document.getElementById('add-user-btn').style.display= 'none';
-    document.getElementById('add-user-form').style.display= 'inline-block';
-}
+let addUser = document.getElementById('adduser')
+let removeUser = document.getElementById('removeuser')
+let newUserOption = document.getElementById('newuseroption')
+
+addUser.addEventListener('click', function(e){
+    let checkInputUserName = document.getElementById('inputusername');
+    if (checkInputUserName===null){
+    let inputUserName = document.createElement('input');
+    inputUserName.id = 'inputusername';
+    inputUserName.autofocus =true;
+    newUserOption.appendChild(inputUserName);
+    newUserOption.submit();
+    }
+})
+
+removeUser.addEventListener('click', function(e){
+    const radios = document.querySelectorAll('input[name="useroption"]')
+    for (const radio of radios){
+        if (radio.checked){
+            let div = radio.parentElement
+            div.remove();
+        }
+    }
+})
