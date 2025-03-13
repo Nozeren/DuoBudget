44e674396ddc4992ab382fb393f5d221e9228a0d
Nozeren <45057714+Nozeren@users.noreply.github.com>
2025-03-13 19:03:06 +0200

    Add remove row in transactions page

--- src/views/static/script/import_file.js
+++ src/views/static/script/import_file.js
@@ -1,28 +1,41 @@
-function addOptions(){
-    fetch('/allusers').then(function(response){
-            return response.json()
-        }).then(function(data){
-            let select = document.getElementById('user');  
-            if (select){
-                for (user of data[0]){
-                    let opt = document.createElement('option');
-                    opt.value = user['id'];
-                    opt.innerHTML = user['name'];
-                    select.appendChild(opt);
-                }
-            }
-        })
-    fetch('/allbanks').then(function(response){
-            return response.json()
-        }).then(function(data){
-            let select = document.getElementById('bank');  
-            if (select){
-                for (bank of data[0]){
-                    let opt = document.createElement('option');
-                    opt.value = bank['id'];
-                    opt.innerHTML = bank['name'];
-                    select.appendChild(opt);
-                }
-            }
-        })
+async function get_all_users() {
+  return fetch("/allusers")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      return data[0];
+    });
+}
+async function get_all_banks() {
+  return fetch("/allbanks")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      return data[0];
+    });
+}
+async function addOptions() {
+  let allUsers = await get_all_users();
+  let select = document.getElementById("user");
+  if (select) {
+    for (user of allUsers) {
+      let opt = document.createElement("option");
+      opt.value = user["id"];
+      opt.innerHTML = user["name"];
+      select.appendChild(opt);
+    }
+  }
+
+  let allBanks = await get_all_banks();
+  let bankSelect = document.getElementById("bank");
+  if (bankSelect) {
+    for (bank of allBanks) {
+      let opt = document.createElement("option");
+      opt.value = bank["id"];
+      opt.innerHTML = bank["name"];
+      bankSelect.appendChild(opt);
+    }
+  }
 }
--- src/views/transactions.py
+++ src/views/transactions.py
@@ -1,26 +1,29 @@
-from flask import  render_template, jsonify, request
+from flask import render_template, jsonify, request
 from views import app
 import requests
 import dotenv
 import os
-import json
 
 dotenv.load_dotenv()
 API_URL = os.getenv('API_URL')
 
+
 @app.route('/transactions', methods=['GET'])
 async def transactions():
     return render_template('transactions/index.html')
+
 
 @app.route('/subcategories', methods=['GET'])
 async def get_subcategories():
     results = requests.get(f'{API_URL}/subcategories/')
     return jsonify(results.json(), results.status_code)
 
+
 @app.route('/alltransactions', methods=['GET'])
 async def get_transactions():
     results = requests.get(f'{API_URL}/transactions/')
     return jsonify(results.json(), results.status_code)
+
 
 @app.route('/updatetransaction', methods=['PUT'])
 async def update_transaction():
@@ -28,14 +31,23 @@ async def update_transaction():
     result = requests.put(f'{API_URL}/transactions/', json=data)
     return jsonify({'result': 'Sucess'}, result.status_code)
 
+
 @app.route('/addtransaction', methods=['POST', 'GET'])
 async def add_transaction():
     data = request.get_json()
-    result = requests.post(f'{API_URL}/transactions/', json={'posted_date':data['posted_date'],
-                                'description':data['description'],
-                                'user_id':data['user_id'],
-                                'bank_id':data['bank_id'],
-                                'subcategory_id':data['subcategory_id'],
-                                'shared_amount':data['shared_amount'], 
-                                'amount':data['amount']})
-    return jsonify(result.json())
+    result = requests.post(f'{API_URL}/transactions/',
+                           json={'posted_date': data['posted_date'],
+                                 'description': data['description'],
+                                 'user_id': data['user_id'],
+                                 'bank_id': data['bank_id'],
+                                 'subcategory_id': data['subcategory_id'],
+                                 'shared_amount': data['shared_amount'],
+                                 'amount': data['amount']})
+    return jsonify(result.json(), result.status_code)
+
+
+@app.route('/deletetransaction', methods=['DELETE'])
+async def delete_transaction():
+    data = request.get_json()
+    result = requests.delete(f'{API_URL}/transactions/{data["id"]}')
+    return jsonify(result.json(), result.status_code)
